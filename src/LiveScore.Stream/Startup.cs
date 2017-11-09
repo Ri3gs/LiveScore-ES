13fe10e9b4f714fa95b6725d5d6e18d84c1f49c7
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2017-11-09 23:58:04 +0200

    removes unused code and views

--- src/LiveScore.Stream/Startup.cs
+++ src/LiveScore.Stream/Startup.cs
@@ -44,7 +44,7 @@ namespace LiveScore.Stream
 			{
 				routes.MapRoute(
 					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
+					template: "{controller=Live}/{action=Index}/{id?}");
 			});
 		}
 	}
--- src/LiveScore.Web/Startup.cs
+++ src/LiveScore.Web/Startup.cs
@@ -6,45 +6,45 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace LiveScore.Web
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
 			services.AddTransient<IMatchControllerService, MatchControllerService>();
-        }
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseStaticFiles();
+			app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Match}/{action=Index}/{id?}");
+			});
 			//BusConfig.Initialize();
-	        //RavenDbConfig.Initialize();
-        }
-    }
+			//RavenDbConfig.Initialize();
+		}
+	}
 }
